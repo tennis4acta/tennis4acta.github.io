1aa2b672993f8d53bb9d75b3656a2fcef8e8e5a0
Xin Zhao <sheenzhaox@users.noreply.github.com>
2019-06-03 11:25:41 +1000

    Update clean-blog.js

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -1056,3 +1056,19 @@ jQuery(document).ready(function($) {
             });
     }
 });
+
+<script src="https://www.eventbrite.com.au/static/widgets/eb_widgets.js"></script>
+
+<script type="text/javascript">
+    var exampleCallback = function() {
+        console.log('Order complete!');
+    };
+
+    window.EBWidgets.createWidget({
+        widgetType: 'checkout',
+        eventId: '62827392422',
+        modal: true,
+        modalTriggerElementId: 'eventbrite-widget-modal-trigger-62827392422',
+        onOrderComplete: exampleCallback
+    });
+</script>
